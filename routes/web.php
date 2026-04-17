627ceb39b5e4e234e079e00a7c328337bf1d0388
muflihanwar81-prog <muflihanwar81@gmail.com>
2026-04-17 07:42:13 +0700

    upp

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-//Route::get('/', function () {
-// return view('welcome');
-//});
+Route::get('/', function () {
+return view('welcome');
+});
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
